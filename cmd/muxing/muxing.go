059767df8706546f29773d5cb967eb7c243a535a
Sasa-saska <99489644+Sasa-saska@users.noreply.github.com>
2022-07-12 17:26:40 +0300

    1

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -10,16 +10,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
-/**
-Please note Start functions is a placeholder for you to start your own solution.
-Feel free to drop gorilla.mux if you want and use any other solution available.
-
-main function reads host/port from env just for an example, flavor it following your taste
-*/
-
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/name/{PARAM}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Fprintf(w, "Hello, %s!", vars["PARAM"])
+	})
+	router.HandleFunc("/bad", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+	router.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		fmt.Fprintf(w, "I got message:\n%s", string(body))
+
+	}).Methods("POST")
+
+	router.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
+		var a, b int
+		a, err := strconv.Atoi(r.Header.Get("a"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err = strconv.Atoi(r.Header.Get("b"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Header().Set(strings.ToLower("a+b"), strconv.Itoa(a+b))
+	}).Methods("POST")
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
