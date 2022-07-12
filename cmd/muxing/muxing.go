9d3120dce7dcc89a88d544ecc68d307254b1b6ec
Sasa-saska <99489644+Sasa-saska@users.noreply.github.com>
2022-07-12 17:32:08 +0300

    2

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -10,40 +10,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{PARAM}", GetParam)
+	router.HandleFunc("/bad", Bad)
+	router.HandleFunc("/data", PostParam)
+	router.HandleFunc("/headers", PostHeaders)
 
-	router.HandleFunc("/name/{PARAM}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		fmt.Fprintf(w, "Hello, %s!", vars["PARAM"])
-	})
-	router.HandleFunc("/bad", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusInternalServerError)
-	})
-	router.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
-		body, _ := ioutil.ReadAll(r.Body)
-		fmt.Fprintf(w, "I got message:\n%s", string(body))
-
-	}).Methods("POST")
-
-	router.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
-		var a, b int
-		a, err := strconv.Atoi(r.Header.Get("a"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		b, err = strconv.Atoi(r.Header.Get("b"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		w.Header().Set(strings.ToLower("a+b"), strconv.Itoa(a+b))
-	}).Methods("POST")
-
-	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
+	fmt.Printf("Starting API server on %s:%d\n", host, port)
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
@@ -54,4 +32,43 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func GetParam(w http.ResponseWriter, r *http.Request) {
+	m := mux.Vars(r) // to take params from url path like this -> /name/{PARAM}
+	value := m["PARAM"]
+	w.Write([]byte(fmt.Sprintf("Hello, %v!", value))) // creates concatination
+}
+
+func Bad(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func PostParam(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte(fmt.Sprintf("I got message:\n%v", string(body))))
+}
+
+func PostHeaders(w http.ResponseWriter, r *http.Request) {
+	headers := r.Header
+	a := headers["A"]
+	b := headers["B"]
+
+	f, err := strconv.Atoi(a[0])
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	s, err := strconv.Atoi(b[0])
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Add("a+b", fmt.Sprintf("%v", f+s))
 }
